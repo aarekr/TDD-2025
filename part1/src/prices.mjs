eda33881c8e53066770483d96c505047c2053c9e
aarekr <aare212@hotmail.com>
2025-03-22 09:04:15 +0100

    datePlain passed to isMonday

--- part1/src/prices.mjs
+++ part1/src/prices.mjs
@@ -76,13 +76,13 @@ function createApp(database) {
 
   function calculateReduction(date, datePlain) {
     let reduction = 0;
-    if (date && isMonday(date) && !isHoliday(date)) {
+    if (datePlain && isMonday(date, datePlain) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
   }
 
-  function isMonday(date) {
+  function isMonday(date, datePlain) {
     return date.getDay() === 1;
   }
 
